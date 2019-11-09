de9ecb00e643eea2107fa2974254c3fc7e0dc32e
João Pedro <joaopedro453@hotmail.com>
2019-11-08 23:09:32 -0300

    App Lista de Tarefas - Marcando tarefas como realizadas

--- tarefa_controller.php
+++ tarefa_controller.php
@@ -44,5 +44,20 @@ if ($acao == 'inserir') {
     $tarefaService->remover();
 
     header('Location: todas_tarefas.php');
+
+} else if ($acao = 'marcarRealizada') {
+    $tarefa = new Tarefa;
+
+    $tarefa->__set('id',$_GET['id'])->__set('id_status', 2);
+
+    $conexao = new Conexao;
+
+    $tarefaService = new TarefaService($conexao, $tarefa);
+    $tarefaService->marcarRealizada(); 
+
+    header('Location: todas_tarefas.php');
+
 }
+
+
 ?>
